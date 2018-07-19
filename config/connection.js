44dacf2a440c416821ca1967282b020b1472d331
Hecky <thechrisheckendorn@gmail.com>
2018-07-18 19:07:40 -0500

    updating connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8880,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+  else {
+      connection = mysql.createConnection({
+      host: 'localhost',
+      port: 8880,
+      user: 'root',
+      password: 'root',
+      database: 'burgers_db'
+      });
+  };
 
 connection.connect(function(err) {
   if (err) {
